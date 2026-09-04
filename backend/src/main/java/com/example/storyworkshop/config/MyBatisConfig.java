4cd2da9247358872a9eb7f5d506031bedc18ec41
fawn <2059389107@qq.com>
2026-09-05 02:02:56 +0800

    Delete backend/src/main/java/com/example/storyworkshop directory

--- backend/src/main/java/com/example/storyworkshop/config/MyBatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.storyworkshop.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan(basePackages = "com.example.storyworkshop.module")
-public class MyBatisConfig {
-}
